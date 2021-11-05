c919045dac0dee1cd91210c62e3d8694a07c2a90
Juan Fernando Lenis <lenisserna2001@gmail.com>
2021-11-05 16:30:16 -0500

    añadí el botón para el modo oscuro y quedo 100% funcional

--- js/main.js
+++ js/main.js
@@ -1,23 +1,47 @@
-load();
-
-const darkbutton = document.querySelector("#dark-mode-button__container");
+const darkb = document.querySelector("#dark-mode-button__container");
 const body = document.querySelector("body");
 
-darkbutton.addEventListener("click", darkmode => {
-  body.classList.toggle("dark-mode");
-  store(body.classList.contains("dark-mode"));
+load();
+
+darkb.addEventListener("click", d => {
+  body.classList.toggle("darkmode");
+  store(body.classList.contains("darkmode"))
 });
 
-function load () {
-  const darkState = localStorage.getItem("dark-mode");
+function load() {
+  const darkmode = localStorage.getItem("darkmode");
 
-  if(!darkState) {
-    store("false")
-  }else if(darkState == "true") {
-    body.classList.add("dark-mode");
+  if(!darkmode) {
+    store("false");
+  }else if(darkmode == "true") {
+    body.classList.add("darkmode");
   }
 }
 
-function store (value) {
-  localStorage.setItem("dark-mode", value)
+function store(value) {
+  localStorage.setItem("darkmode", value);
 }
+
+// load();
+
+// const darkbutton = document.querySelector("#dark-mode-button__container");
+// const body = document.querySelector("body");
+
+// darkbutton.addEventListener("click", darkmode => {
+//   body.classList.toggle("dark-mode");
+//   store(body.classList.contains("dark-mode"));
+// });
+
+// function load () {
+//   const darkState = localStorage.getItem("dark-mode");
+
+//   if(!darkState) {
+//     store("false")
+//   }else if(darkState == "true") {
+//     body.classList.add("dark-mode");
+//   }
+// }
+
+// function store (value) {
+//   localStorage.setItem("dark-mode", value)
+// }
