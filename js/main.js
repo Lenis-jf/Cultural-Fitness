679cde364f35020f655468dcf9c50f99e86e47ee
Juan Fernando Lenis <lenisserna2001@gmail.com>
2021-12-10 23:28:25 +0100

    comenzando la calculadora (seccion JS)

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ const darkb = document.querySelector("#dark-mode-button__container");
 const darkb2 = document.querySelector("#dark-mode-button__container--id");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
+var height = document.getElementById("height");
+var age = document.getElementById("age");
+var tmb = document.getElementById("tmb");
+var exercise = document.getElementById("exercise");
+const calculate = document.getElementById("calculate")
 
 load();
 
 main.addEventListener("click", darClick);
+calculate.addEventListener("click", darValor)
 
 darkb.addEventListener("click", d => {
   body.classList.toggle("darkmode");
@@ -37,4 +43,9 @@ function darClick() {
   if(menu) {
   var menu = document.getElementById("check").click();
   }
+}
+
+function darValor() {
+  let weightValue = document.getElementById("weight").value;
+  document.getElementById("calculator-answer-1").innerHTML = weightValue;
 }
