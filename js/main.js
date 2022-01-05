174099b404ec2afcf9d1ec23bc83f99a6eb05050
Juan Fernando Lenis <lenisserna2001@gmail.com>
2022-01-06 00:56:35 +0100

    finalice la funcionalidad del advice con Javascript

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ const darkb = document.querySelector("#dark-mode-button__container");
 const darkb2 = document.querySelector("#dark-mode-button__container--id");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
-const calculate = document.getElementById("calculate")
-const calculateAdelgazar = document.getElementById("calculate-2")
-const calculateCrecer = document.getElementById("calculate-3")
+const calculate = document.getElementById("calculate");
+const calculateAdelgazar = document.getElementById("calculate-2");
+const calculateCrecer = document.getElementById("calculate-3");
+const continue_button = document.getElementById("continue-button");
 var tmbValue;
 var plusValue;
 
@@ -53,6 +54,21 @@ function radioCheck() {
   return plusValue;
 }
 
+function blurry() {
+  document.getElementById("blur-1").classList.toggle("blur");
+  document.getElementById("blur-2").classList.toggle("blur");
+}
+
+
+function advice() {
+  document.getElementById("alert-container").style.display="block";
+}
+
+function adviceDisappear() {
+  document.getElementById("alert-container").style.display="none";
+  blurry();
+}
+
 calculate.addEventListener("click", calcularTmb);
 
 function calcularTmb() {
@@ -60,12 +76,18 @@ function calcularTmb() {
   let heightValue = document.getElementById("height").value;
   let ageValue = document.getElementById("age").value;
 
+  blurry();
+
   radioCheck();
 
-  tmbValue = (10 * weightValue) + (6.25 * heightValue) - (5 * ageValue) + plusValue;
+  advice()
 
-  document.getElementById("calculator-answer-tmb").innerHTML = "Tu tasa metabólica basal es de: " + tmbValue + " Cal";
+  tmbValue = Math.round((10 * weightValue) + (6.25 * heightValue) - (5 * ageValue) + plusValue);
+
+  document.getElementById("answer-space").innerHTML = "Tu tasa metabólica basal es de: " + tmbValue + " Cal";
 }
+
+continue_button.addEventListener("click", adviceDisappear);
 
 calculateAdelgazar.addEventListener("click", adelgazar);
 
@@ -74,32 +96,38 @@ function adelgazar() {
   let exerciseOption = document.getElementById("exercise").value;
   var calParaAdelgazar;
 
+  blurry();
+
+  radioCheck();
+
+  advice()
+
   switch (exerciseOption) {
     case "Atleta":
-      var calParaAdelgazar = (tmbFinalValue * 1.9) - 500;
-      document.getElementById("calculator-answer-adelgazar").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
+      var calParaAdelgazar = Math.round((tmbFinalValue * 1.9) - 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
     break;
     case "Mucho":
-      var calParaAdelgazar = (tmbFinalValue * 1.72) - 500;
-      document.getElementById("calculator-answer-adelgazar").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
+      var calParaAdelgazar = Math.round((tmbFinalValue * 1.72) - 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
     break;
     case "Medio":
-      var calParaAdelgazar = (tmbFinalValue * 1.55) - 500;
-      document.getElementById("calculator-answer-adelgazar").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
+      var calParaAdelgazar = Math.round((tmbFinalValue * 1.55) - 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
     break;
     case "Ligero":
-      var calParaAdelgazar = (tmbFinalValue * 1.375) - 500;
-      document.getElementById("calculator-answer-adelgazar").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
+      var calParaAdelgazar = Math.round((tmbFinalValue * 1.375) - 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
     break;
     case "Poco":
-      var calParaAdelgazar = (tmbFinalValue * 1.2) - 500;
-      document.getElementById("calculator-answer-adelgazar").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
+      var calParaAdelgazar = Math.round((tmbFinalValue * 1.2) - 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaAdelgazar + " Cal aproximadamente para adelgazar";
     break;
     case "Nada":
-      document.getElementById("calculator-answer-adelgazar").innerHTML = "No te rindas, levantate y cambia tu vida, ¡nosotros creemos en ti!";
+      document.getElementById("answer-space").innerHTML = "No te rindas, levantate y cambia tu vida, ¡nosotros creemos en ti!";
     break;
     default:
-      document.getElementById("calculator-answer-adelgazar").innerHTML = "Por favor escoge una de las opciones anteriores"
+      document.getElementById("answer-space").innerHTML = "Por favor escoge una de las opciones anteriores"
   }
 }
 
@@ -109,35 +137,38 @@ function crecer() {
   const tmbFinalValue = document.getElementById("tmb-2").value;
   let exerciseOption = document.getElementById("exercise-2").value;
   var calParaCrecer;
+
+  blurry();
+
+  radioCheck();
+
+  advice()
+
   switch (exerciseOption) {
     case "Atleta":
-      var calParaCrecer = (tmbFinalValue * 1.9) + 500;
-      document.getElementById("calculator-answer-crecer").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
+      var calParaCrecer = Math.round((tmbFinalValue * 1.9) + 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
     break;
     case "Mucho":
-      var calParaCrecer = (tmbFinalValue * 1.72) + 500;
-      document.getElementById("calculator-answer-crecer").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
+      var calParaCrecer = Math.round((tmbFinalValue * 1.72) + 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
     break;
     case "Medio":
-      var calParaCrecer = (tmbFinalValue * 1.55) + 500;
-      document.getElementById("calculator-answer-crecer").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
+      var calParaCrecer = Math.round((tmbFinalValue * 1.55) + 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
     break;
     case "Ligero":
-      var calParaCrecer = (tmbFinalValue * 1.375) + 500;
-      document.getElementById("calculator-answer-crecer").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
+      var calParaCrecer = Math.round((tmbFinalValue * 1.375) + 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
     break;
     case "Poco":
-      var calParaCrecer = (tmbFinalValue * 1.2) + 500;
-      document.getElementById("calculator-answer-crecer").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
+      var calParaCrecer = Math.round((tmbFinalValue * 1.2) + 500);
+      document.getElementById("answer-space").innerHTML = "Deberías consumir " + calParaCrecer + " Cal aproximadamente para crecer";
     break;
     case "Nada":
-      document.getElementById("calculator-answer-crecer").innerHTML = "No te rindas, levantate y cambia tu vida, ¡nosotros creemos en ti!";
+      document.getElementById("answer-space").innerHTML = "No te rindas, levantate y cambia tu vida, ¡nosotros creemos en ti!";
     break;
     default:
-      document.getElementById("calculator-answer-crecer").innerHTML = "Por favor escoge una de las opciones anteriores"
+      document.getElementById("answer-space").innerHTML = "Por favor escoge una de las opciones anteriores"
   }
-}
-
-function alertAdvice(message) {
-  
 }
